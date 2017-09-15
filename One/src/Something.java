7fab1769097995179d26c2669048d02b90859485
BAMMCAM <30834136+BAMMCAM@users.noreply.github.com>
2017-09-15 09:40:01 +0100

    test sel

--- One/src/Something.java
+++ One/src/Something.java
@@ -1,9 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Something {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hi");
+			
+			System.setProperty("webdriver.chrome.driver", "c:\\dev\\chromedriver\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			
+			driver.get("http://www.google.co.uk");
+			
 	}
 
 }
